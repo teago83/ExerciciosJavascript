be63018b9d52eec3063a342f4e3a898bc096931f
teago83 <supremeleaderpastel@gmail.com>
2020-04-01 22:33:32 -0300

    Mais alguns exercícios foram feitos.

--- Lista1/A.js
+++
@@ -1,3 +0,0 @@
-/*a) Elabore um script usando a função console que leia um valor e imprima os resultados "É maior que 10", "Não é maior que 10" ou "É igual a 10", 
-dependendo do número que for digitado pelo usuário.*/
-
